3c5d8467b48491f9110486c9106e762dd6a2c4bc
AdarShaked <adar@cloudify.co>
2020-08-20 11:17:39 +0300

    implmented helm sdk and tests

---
+++ helm_sdk/__init__.py
@@ -0,0 +1,76 @@
+########
+# Copyright (c) 2018-2020 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from helm_sdk.utils import (run_subprocess, prepare_set_parameter,
+                            prepare_parameter)
+import json
+
+
+class Helm(object):
+
+    def __init__(self,
+                 logger,
+                 binary_path,
+                 environment_variables
+                 ):
+        self.binary_path = binary_path
+        self.logger = logger
+        if not isinstance(environment_variables, dict):
+            raise Exception(
+                "Unexpected type for environment variables (should be a "
+                "dict): {0}".format(type(
+                    environment_variables)))
+
+        self.env = environment_variables
+
+    def execute(self, command, return_output=False):
+        return run_subprocess(
+            command, self.logger,
+            cwd=None,
+            additional_env=self.env,
+            additional_args=None,
+            return_output=return_output)
+
+    def _helm_command(self, args):
+        cmd = [self.binary_path]
+        cmd.extend(args)
+        return cmd
+
+    def install(self, release_name, chart, flags, set_arguments=None):
+        cmd = ['install', release_name, chart, '--wait', '--output=json']
+        cmd.extend(map(prepare_parameter, flags))
+        set_arguments = set_arguments or []
+        cmd.extend(map(prepare_set_parameter, set_arguments))
+        output = self.execute(self._helm_command(cmd), True)
+        return json.loads(output)
+
+    def uninstall(self, release_name, flags=None):
+        cmd = ['uninstall', release_name]
+        flags = flags or []
+        cmd.extend(map(prepare_parameter, flags))
+        return self.execute(self._helm_command(cmd))
+
+    def repo_add(self, repo_name, repo_url, flags=None):
+        cmd = ['repo', 'add', repo_name, repo_url]
+        flags = flags or []
+        cmd.extend(map(prepare_parameter, flags))
+        return self.execute(self._helm_command(cmd))
+
+    def repo_remove(self, repo_name, flags=None):
+        cmd = ['repo', 'remove', repo_name]
+        flags = flags or []
+        cmd.extend(map(prepare_parameter, flags))
+        return self.execute(self._helm_command(cmd))
---
+++ helm_sdk/utils.py
@@ -0,0 +1,138 @@
+########
+# Copyright (c) 2018-2020 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import copy
+import subprocess
+import os
+import threading
+
+from helm_sdk._compat import StringIO, text_type
+from helm_sdk.exceptions import CloudifyHelmSDKError
+
+
+def run_subprocess(command, logger, cwd=None,
+                   additional_env=None,
+                   additional_args=None, return_output=False):
+    if additional_args is None:
+        additional_args = {}
+    args_to_pass = copy.deepcopy(additional_args)
+    if additional_env:
+        passed_env = args_to_pass.setdefault('env', {})
+        passed_env.update(os.environ)
+        passed_env.update(additional_env)
+
+    logger.info("Running: command=%s, cwd=%s, additional_args=%s",
+                command, cwd, args_to_pass)
+    process = subprocess.Popen(
+        args=command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        stdin=None,
+        cwd=cwd,
+        **args_to_pass)
+
+    if return_output:
+        stdout_consumer = CapturingOutputConsumer(
+            process.stdout)
+    else:
+        stdout_consumer = LoggingOutputConsumer(
+            process.stdout, logger, "<out> ")
+    stderr_consumer = LoggingOutputConsumer(
+        process.stderr, logger, "<err> ")
+
+    return_code = process.wait()
+    stdout_consumer.join()
+    stderr_consumer.join()
+
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, command)
+
+    output = stdout_consumer.buffer.getvalue() if return_output else None
+    logger.info("Returning output:\n%s",
+                output if output is not None else '<None>')
+    return output
+
+
+# Stolen from the script plugin, until this class
+# moves to a utils module in cloudify-common.
+class OutputConsumer(object):
+    def __init__(self, out):
+        self.out = out
+        self.consumer = threading.Thread(target=self.consume_output)
+        self.consumer.daemon = True
+
+    def consume_output(self):
+        for line in self.out:
+            self.handle_line(line)
+        self.out.close()
+
+    def handle_line(self, line):
+        raise NotImplementedError("Must be implemented by subclass")
+
+    def join(self):
+        self.consumer.join()
+
+
+class LoggingOutputConsumer(OutputConsumer):
+    def __init__(self, out, logger, prefix):
+        OutputConsumer.__init__(self, out)
+        self.logger = logger
+        self.prefix = prefix
+        self.consumer.start()
+
+    def handle_line(self, line):
+        self.logger.info(
+            "{0}{1}".format(text_type(self.prefix),
+                            line.decode('utf-8').rstrip('\n')))
+
+
+class CapturingOutputConsumer(OutputConsumer):
+    def __init__(self, out):
+        OutputConsumer.__init__(self, out)
+        self.buffer = StringIO()
+        self.consumer.start()
+
+    def handle_line(self, line):
+        self.buffer.write(line.decode('utf-8'))
+
+    def get_buffer(self):
+        return self.buffer
+
+
+def prepare_parameter(arg_dict):
+    """ 
+    Prepare single parameter.
+    :param arg_dict: dictionary with the name of the flag and value(optional)
+    :return: "--name=value" or -"-name"
+    """
+    try:
+        param_string = "--" + arg_dict["name"]
+        return param_string + '=' + arg_dict.get("value") if arg_dict.get(
+            "value") else param_string
+    except KeyError:
+        raise CloudifyHelmSDKError("parameter name doesen't exist")
+
+
+def prepare_set_parameter(set_dict):
+    """
+    Prepare single set parameter.
+    :param set_dict: dictionary with the name of the variable to set command
+    and its value
+    :return "--set name=value"
+    """
+    try:
+        return "--set " + set_dict["name"] + "=" + set_dict["value"]
+    except KeyError:
+        raise CloudifyHelmSDKError("set parameter name or value is missing")
