02b02bf830f9b4fbeb76d1c3d57fa66d2b5372a6
AdarShaked <adar@cloudify.co>
2020-09-27 13:01:26 +0300

    exceptions messages typo fixes

--- helm_sdk/utils.py
+++ helm_sdk/utils.py
@@ -91,7 +91,7 @@ class OutputConsumer(object):
         self.out.close()
 
     def handle_line(self, line):
-        raise NotImplementedError("Must be implemented by subclass")
+        raise NotImplementedError("Must be implemented by subclass.")
 
     def join(self):
         self.consumer.join()
@@ -135,7 +135,7 @@ def prepare_parameter(arg_dict):
         return param_string + '=' + arg_dict.get("value") if arg_dict.get(
             "value") else param_string
     except KeyError:
-        raise CloudifyHelmSDKError("parameter name doesen't exist")
+        raise CloudifyHelmSDKError("Parameter name doesen't exist.")
 
 
 def prepare_set_parameters(set_values):
@@ -152,7 +152,7 @@ def prepare_set_parameters(set_values):
             set_list.append(set_dict["name"] + "=" + set_dict["value"])
         except KeyError:
             raise CloudifyHelmSDKError(
-                "set parameter name or value is missing")
+                "Set parameter name or value is missing.")
     return set_list
 
 
@@ -160,5 +160,5 @@ def validate_no_collisions_between_params_and_flags(flags):
     if [flag for flag in flags if flag['name'] in FLAGS_LIST_TO_VALIDATE]:
         raise CloudifyHelmSDKError(
             'Please do not pass {flags_list} under "flags" property,'
-            'each of them has a known property'.format(
+            'each of them has a known property.'.format(
                 flags_list=FLAGS_LIST_TO_VALIDATE))
