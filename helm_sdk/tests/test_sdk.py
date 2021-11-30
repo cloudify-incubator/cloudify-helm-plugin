484c4f06028a7826e430bcb400b55a6aaf7cf603
EarthmanT <trammell@cloudify.co>
2021-11-30 09:19:22 -0500

    flake 8 issue

--- helm_sdk/tests/test_sdk.py
+++ helm_sdk/tests/test_sdk.py
@@ -130,9 +130,9 @@ class HelmSDKTest(HelmTestBase):
                                 mock_set_args,
                                 kubeconfig='/path/to/config')
         cmd_expected = [[HELM_BINARY, 'upgrade', 'release1', 'my_chart',
-                        '--atomic', '-o=json', '--kubeconfig=/path/to/config',
-                        '--dry-run', '--timeout=100', '--set', 'x=y', '--set',
-                        'a=b'], 'additional_args=None', 'return_output=True']
+                         '--atomic', '-o=json', '--kubeconfig=/path/to/config',
+                         '--dry-run', '--timeout=100', '--set', 'x=y', '--set',
+                         'a=b'], 'additional_args=None', 'return_output=True']
         mock_execute.assert_called_once_with(*cmd_expected, True)
         self.assertEqual(out, {"name": "release1"})
 
