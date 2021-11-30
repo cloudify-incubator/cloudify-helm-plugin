510a6a6c3bd9984900aa8b7e0647d3e3f589d6dc
nely <nely@cloudify.co>
2021-11-30 17:20:37 +0200

    fix test_tasks py36 2

--- cloudify_helm/tests/test_tasks.py
+++ cloudify_helm/tests/test_tasks.py
@@ -194,7 +194,8 @@ class TestTasks(TestBase):
                     name="stable",
                     repo_url="https://kubernetes-charts.storage.googleapis"
                              ".com/",
-                    flags=[])
+                    flags=[],
+                    additional_args={'max_sleep_time': 300})
 
     def test_add_repo_use_external_resource(self):
         properties = {
@@ -257,8 +258,7 @@ class TestTasks(TestBase):
                     repo_url="https://kubernetes-charts.storage.googleapis"
                              ".com/",
                     flags=[],
-                    additional_args="{'max_sleep_time': 300}"
-                )
+                    additional_args={'max_sleep_time': 300})
 
     def test_install_release(self):
         properties = self.mock_install_release_properties()
